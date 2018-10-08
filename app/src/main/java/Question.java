fc32bcff17dc0296c57d89718f67787532703ba4
JennaN23 <jennan@outlook.com>
2018-10-08 09:36:01 -0700

    Initial commit

---
+++ app/src/main/java/Question.java
@@ -0,0 +1,30 @@
+public class Question {
+    private String question;
+    private boolean answer;
+
+    public boolean checkAnswer(boolean userAnswer)
+    {
+        if(answer != userAnswer){
+            return false;
+        }
+        else{
+        return true;
+        }
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public boolean isAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(boolean answer) {
+        this.answer = answer;
+    }
+}
